7f7ce002a59c06ed306c5cad85a6c76c2e3f6586
Алтай <altosha211@gmail.com>
2021-04-05 20:43:06 +0600

    Bug fixed

--- tutorial/tutorial/snippets/urls.py
+++ tutorial/tutorial/snippets/urls.py
@@ -4,7 +4,7 @@ from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
-router.register(r'snippets', views.UserViewSEt)
+router.register(r'snippets', views.UserViewSet)
 
 
 urlpatterns = [
