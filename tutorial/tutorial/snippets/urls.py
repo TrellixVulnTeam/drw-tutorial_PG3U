4e734da26ee8d2043fd7cd15ad86e45765563792
Алтай <altosha211@gmail.com>
2021-04-05 20:39:35 +0600

    Completed sixth lesson DRF

--- tutorial/tutorial/snippets/urls.py
+++ tutorial/tutorial/snippets/urls.py
@@ -1,14 +1,12 @@
-from django.urls import path
-from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path, include
 from snippets import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet)
+router.register(r'snippets', views.UserViewSEt)
+
 
 urlpatterns = [
-    path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
-    path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
-    path('users/', views.UserList.as_view(), name='user-list'),
-    path('users/<int:pk>/', views.UserDetail.as_view(), name='user-detail'),
-    path('', views.api_root),
-    path('snippets/<int:pk>/highlight', views.SnippetHighlight.as_view(), name='snippet-highlight'),
+    path('', include(router.urls))
 ]
-
-urlpatterns = format_suffix_patterns(urlpatterns)
